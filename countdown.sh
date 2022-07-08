7ca54bc327de11ebf7d23b777b299c2fe2e9ce87
Kelly Young <kelpopko@gmail.com>
2022-07-08 10:24:24 -0500

    Replace for loop with while loop

--- countdown.sh
+++ countdown.sh
@@ -1,14 +1,33 @@
 #!/bin/bash
 
-# Program that counts down to zero from a given argument
+# Program that counts down to zero from a given argument - FOR loop
+
+# echo -e "\n~~ Countdown Timer ~~\n"
+
+# if [[ $1 -gt 0 ]]
+# then 
+#   for (( i = $1; i > 0; i-- ))
+#   do
+#     echo $i
+#     sleep 1
+#   done
+# else 
+#   echo Include a positive integer as the first argument.
+# fi
+
+
+# Program that counts down to zero from a given argument - WHILE loop
+echo -e "\n~~ Countdown Timer ~~\n"
 
 if [[ $1 -gt 0 ]]
-then 
-  for (( i = $1; i > 0; i-- ))
+then
+  I=$1
+  while [[ $I -ge 0 ]]
   do
-    echo $i
+    echo $I
+    (( I-- ))
     sleep 1
   done
-else 
+else
   echo Include a positive integer as the first argument.
 fi
