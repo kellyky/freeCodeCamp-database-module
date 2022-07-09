3c0ca71ec7b01f10eea9cca33e8c4750b38ae4b5
Kelly Young <kelpopko@gmail.com>
2022-07-09 01:13:17 -0500

    Refactor: use echo-pause function so I can adjust pause timing more easily

--- text-adventure.sh
+++ text-adventure.sh
@@ -19,64 +19,43 @@ function INTRO (){
   ECHO_PAUSE "Rumor has it that a $VILLAIN is somewhere around here, and has been terrifying the nearby villagers."
   ECHO_PAUSE "In front of you is a house."
   ECHO_PAUSE "In your hand you hold your trusty (but ineffective, given the circumstances) $WIMPY_WEAPON.\n"
-
-
-
-  # echo You find yourself standing in a meadow with grass and wild flowers.
-  # sleep 0.5
-  # echo Rumor has it that a $VILLAIN is somewhere around here, and has been terrifying the nearby villagers.
-  # sleep 0.5
-  # echo In front of you is a house.
-  # sleep 0.5
-  # echo -e "In your hand you hold your trusty (but ineffective, given the circumstances) $WIMPY_WEAPON.\n"
-  # sleep 0.5
 }
 
 function CHOOSE_HOUSE_OR_CAVE () {
-  echo Enter 1 to knock on the door of the house.
-  sleep 0.5
-  echo Enter 2 to peer into the cave.
-  sleep 0.5
-  echo What would you like to do?
-  sleep 0.5
-  echo "(Please enter 1 or 2.)"
+  ECHO_PAUSE "Enter 1 to knock on the door of the house."
+  ECHO_PAUSE "Enter 2 to peer into the cave."
+  ECHO_PAUSE "What would you like to do?"
+  ECHO_PAUSE "(Please enter 1 or 2.)"
   read CHOICE  
   if [[ $CHOICE == 1 ]] 
     then KNOCK_DOOR 
   elif [[ $CHOICE == 2 ]]
     then CHOOSE_CAVE 
   else 
-    echo That\'s not an option.
-    sleep 0.5
-    CHOOSE_HOUSE_OR_CAVE  # restate the choices
+    ECHO_PAUSE "That\'s not an option."
+    CHOOSE_HOUSE_OR_CAVE 
   fi
 }
 
 function KNOCK_DOOR () {
-  echo You approach the door of the house.
-  sleep 0.5
-  echo You are about to enter and out steps the $VILLAIN.
-  sleep 0.5
-  echo Eep! This is the $VILLAIN\'s house!
-  sleep 0.5
-  echo The $VILLAIN attacks you!
-  sleep 0.5
+  ECHO_PAUSE "You approach the door of the house."
+  ECHO_PAUSE "You are about to enter and out steps the $VILLAIN."
+  ECHO_PAUSE "Eep! This is the $VILLAIN's house!"
+  ECHO_PAUSE "The $VILLAIN attacks you!"
   if [[ $ARMED_WITH != $MIGHTY_WEAPON ]]
-    then echo You feel a little under-prepared, what with only having a $WIMPY_WEAPON.
+    then ECHO_PAUSE "You feel a little under-prepared, what with only having a $WIMPY_WEAPON."
   fi
-  echo "Would you like to (1) fight or (2) run away?"
+  ECHO_PAUSE "Would you like to (1) fight or (2) run away?"
   read CHOICE
   if [[ $CHOICE == 1 ]]
     then CHOOSE_FIGHT
   elif [[ $CHOICE == 2 ]]
-    then echo You run back into the field.
-    sleep 0.5
-    echo -e "Luckily, you don't seem to have been followed.\n"
-    sleep 0.5
-    CHOOSE_HOUSE_OR_CAVE
+    then 
+      ECHO_PAUSE "You run back into the field."
+      ECHO_PAUSE "Luckily, you don't seem to have been followed.\n"
+      CHOOSE_HOUSE_OR_CAVE
   else 
-    echo That\'s not an option.
-    sleep 0.5
+    ECHO_PAUSE "That's not an option."
     CHOOSE_HOUSE_OR_CAVE
   fi
 }
@@ -84,22 +63,16 @@ function KNOCK_DOOR () {
 function CHOOSE_CAVE () {
   if [[ $ARMED_WITH == $MIGHTY_WEAPON ]]
   then 
-    echo -e "You've been here before, and gotten all the good stuff. It's just an empty cave now. You walk back out to the field.\n"
-    sleep 0.5
+    ECHO_PAUSE "You've been here before, and gotten all the good stuff. It's just an empty cave now. You walk back out to the field.\n"
     CHOOSE_HOUSE_OR_CAVE
   else  
-    echo You peer cautiously into the cave.
-    sleep 0.5
-    echo It turns out to be a very small cave.
-    sleep 0.5
-    echo Your eye catches a glint of metal behind a rock.
-    sleep 0.5
-    echo You have found the $MIGHTY_WEAPON!
-    sleep 0.5
+    ECHO_PAUSE "You peer cautiously into the cave."
+    ECHO_PAUSE "It turns out to be a very small cave."
+    ECHO_PAUSE "Your eye catches a glint of metal behind a rock."
+    ECHO_PAUSE "You have found the $MIGHTY_WEAPON!"
     ARMED_WITH+=$MIGHTY_WEAPON
-    echo You discard your silly $WIMPY_WEAPON and take the sword with you.
-    sleep 0.5
-    echo -e "You walk back out to the field.\n"
+    ECHO_PAUSE "You discard your silly $WIMPY_WEAPON and take the sword with you."
+    ECHO_PAUSE "You walk back out to the field.\n"
     CHOOSE_HOUSE_OR_CAVE
   fi
 }
@@ -107,22 +80,15 @@ function CHOOSE_CAVE () {
 function CHOOSE_FIGHT (){ 
   if [[ $ARMED_WITH == $MIGHTY_WEAPON ]]
     then
-      echo As the $VILLAIN moves to attach, you ready your new weapon. 
-      sleep 0.5
-      echo -e "$ARMED_WITH shines brightly in your hand as you brace yourself for the attach."
-      sleep 0.5
-      echo But the $VILLAIN takes one look at your shiny new toy and runs away!
-      sleep 0.5
-      echo You have rid the town of the $VILLAIN. You are victorious!
-      sleep 0.5
+      ECHO_PAUSE "As the $VILLAIN moves to attach, you ready your new weapon." 
+      ECHO_PAUSE "$ARMED_WITH shines brightly in your hand as you brace yourself for the attach."
+      ECHO_PAUSE "But the $VILLAIN takes one look at your shiny new toy and runs away!"
+      ECHO_PAUSE "You have rid the town of the $VILLAIN. You are victorious!"
       WANT_TO_PLAY_AGAIN
   else
-    echo You do your best...
-    sleep 0.5
-    echo "but your $WIMPY_WEAPON is no match for the $VILLAIN."
-    sleep 0.5
-    echo You have been defeated!
-    sleep 0.5
+    ECHO_PAUSE "You do your best..."
+    ECHO_PAUSE "but your $WIMPY_WEAPON is no match for the $VILLAIN."
+    ECHO_PAUSE "You have been defeated!"
     WANT_TO_PLAY_AGAIN
   fi
 }
